de0668168be354eaee7291eafe66c106290d1787
developerteam888 <developerteam888@outlook.com>
2020-02-22 11:47:53 -0500

    add movment

---
+++ Assets/Scripts/GameMoveController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMoveController : MonoBehaviour
+{
+    //public OVRInput.Controller Controller = OVRInput.Controller.RTouch;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(TrailProducer.Instance.canmove)
+        {
+            transform.Translate(Input.GetAxis("Horizontal") * Time.smoothDeltaTime * 25, 0, Input.GetAxis("Vertical") * Time.smoothDeltaTime * 25, Space.Self);
+         //   transform.Translate(OVRInput.Get(OVRInput.Axis1D) * Time.smoothDeltaTime * 25, 0, Input.GetAxis("Vertical") * Time.smoothDeltaTime * 25, Space.Self);
+        }
+    }
+}
